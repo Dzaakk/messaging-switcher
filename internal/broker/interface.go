185fd3f8fd37bed15bea05efccb7e56ec58b56af
Dzkzzz <dzdzaakk@gmail.com>
2025-09-29 22:39:52 +0700

    setup interface

---
+++ internal/broker/interface.go
@@ -0,0 +1,20 @@
+package broker
+
+import "context"
+
+type Message struct {
+	Key     []byte
+	Value   []byte
+	Headers map[string]string
+
+	Ack  func() error
+	Nack func() error
+}
+
+type Handler func(msg Message) error
+
+type Broker interface {
+	Publis(topic string, key []byte, value []byte, headers map[string]string) error
+	Consume(ctx context.Context, topic string, group string, handler Handler) error
+	Close() error
+}
